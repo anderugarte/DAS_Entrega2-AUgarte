f58876417b3bd48b2e32fb86e2fad99b8c074cb9
ander <anderugarte1999@gmail.com>
2021-04-21 00:12:00 +0200

    Mejoradas las interfaces de Inicio Sesion y Registro

--- app/src/main/java/com/example/entrega2_das/InicioRegistro/InicioSesion.java
+++ app/src/main/java/com/example/entrega2_das/InicioRegistro/InicioSesion.java
@@ -2,7 +2,11 @@ package com.example.entrega2_das.InicioRegistro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.entrega2_das.R;
 
@@ -12,5 +16,32 @@ public class InicioSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        EditText tUsername = (EditText) findViewById(R.id.ptUsername);
+        EditText tPassword = (EditText) findViewById(R.id.ptPassword);
+        Button bIniSes = (Button) findViewById(R.id.bIniSes);
+        Button bRegis = (Button) findViewById(R.id.bRegis);
+
+        // Se realizan las comprobaciones para el inicio de sesion
+        bIniSes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Se recogen los campos rellenados por el usuario
+                String user = tUsername.getText().toString();
+                String pass = tPassword.getText().toString();
+
+            }
+        });
+
+        // Se redirecciona el usuario a la interfaz de registro
+        bRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent re = new Intent (getBaseContext(), Registro.class);
+                startActivity(re);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/entrega2_das/InicioRegistro/Registro.java
+++ app/src/main/java/com/example/entrega2_das/InicioRegistro/Registro.java
@@ -3,7 +3,11 @@ package com.example.entrega2_das.InicioRegistro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.entrega2_das.Principal.ClaseDialogoFecha;
 import com.example.entrega2_das.R;
 
 public class Registro extends AppCompatActivity {
@@ -12,5 +16,48 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        // A traves de esta clase se gestionara el registro de un nuevo usuario
+        EditText nombre = (EditText) findViewById(R.id.ptNombre);
+        EditText apellidos = (EditText) findViewById(R.id.ptApellidos);
+        EditText username = (EditText) findViewById(R.id.ptUsername);
+        EditText contrasena = (EditText) findViewById(R.id.ptPassword);
+        EditText mostrarC = (EditText) findViewById(R.id.ptCumple);
+        Button bRegistro = (Button) findViewById(R.id.bReg);
+
+        // Al pulsar este EditText, desplegaremos un dialogo donde se podra seleccionar la fecha de nacimientop del usuario
+        mostrarC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.ptCumple:
+                        showDatePickerDialog(mostrarC);
+                        break;
+                }
+            }
+        });
+
+        // Al pulsar este boton, se gestionara el registro del usuario
+        bRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Obtenemos los campos introducidos por el usuario
+                String n = nombre.getText().toString();
+                String a = apellidos.getText().toString();
+                String u = username.getText().toString();
+                String p = contrasena.getText().toString();
+                String d = mostrarC.getText().toString();
+
+            }
+        });
     }
+
+    // Este metodo nos ayuda a desplegar el dialogo para la seleccion de la fecha de nacimiento y nos permite enviar
+    // el EditText por parametro para una vez obtenida la fecha poder realizar un .setText("fecha") en el
+    private void showDatePickerDialog(final EditText editText) {
+        ClaseDialogoFecha dialogoCumpleanos = new ClaseDialogoFecha(editText);
+        dialogoCumpleanos.show(getSupportFragmentManager(),"cumple");
+    }
+
 }
